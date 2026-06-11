ba555ada08d940767bfea992e89bf7d55522e346
Akhil <akhil@email.com>
2026-06-11 15:10:17 +0530

    second commit

--- 8. Arrays & Nested Arrays .js
+++ 8. Arrays & Nested Arrays .js
@@ -9,3 +9,7 @@ var myArray = [["Bulls", 23], ["White Sox", 45]];
 //Accessing array data with indexes
 var ourArray = [50, 60, 70];
 var ourData = ourArray[0];
+
+//Modifying array data with indexes
+var ourArray = [18, 64, 99];
+ourArray[1] = 45; //ourArray now equals [18, 45, 99]
