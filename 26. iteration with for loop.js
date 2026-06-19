73b70a87854246676fa7d6e39c4cd8ec723998a6
Akhil <akhil@email.com>
2026-06-19 19:44:39 +0530

    day 5

---
+++ 26. iteration with for loop.js
@@ -0,0 +1,21 @@
+//pushing even nums to array
+var evenNums=[];
+for(var i=0;i<10;i+=2){
+    evenNums.push(i);
+}
+console.log(evenNums);
+
+//pushing odd nums to array
+var oddNums=[];
+for(var i=1;i<10;i+=2){
+    oddNums.push(i);
+}
+console.log(oddNums);
+
+//counting backwards
+var countBack=[];
+for(var i=10;i>0;i--){
+    countBack.push(i);
+}
+console.log(countBack);
+
